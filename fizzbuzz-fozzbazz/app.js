a7ac9cc9699dfc535c673654fb384c0242f7b85b
KAM9077 <kamalhajjar907@gmail.com>
2017-02-05 09:20:18 +0200

    tring to solve

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -18,6 +18,21 @@ angular.module('app', ['ngRoute'])
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
 
-  $routeProvider.factory('counter', function($http){})
-  .controller('fizzbuzzCtrl', function(){})
-  .controller('fozzbazzCtrl', function(){});
+  $routeProvider.factory('counter', function($http){
+  
+
+    }
+  })
+  .controller('fizzbuzzCtrl', function($scope){
+    $scope.counterbazz = 0;
+    $scope.increment = function(){
+      $scope.counterbazz++;
+    }
+  
+  })
+  fozzbazzCtrl.controller('fozzbazzCtrl', function($scope){
+    $scope.counterbuzz = 0;
+    $scope.increment = function(){
+      $scope.counterbuzz++;
+    }
+  });
